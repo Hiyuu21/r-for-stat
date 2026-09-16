c666a2fb9c06273b400774b9e99696ef8ad6fbd4
Hiyuu <zhark1221@gmail.com>
2026-09-16 19:11:15 +0800

    Time plot for tutorials

--- time_plot.R
+++ time_plot.R
@@ -1,4 +1,6 @@
+# -----------------------------------------
 # Tutorial 5 Q1
+# -----------------------------------------
 
 y <- c(1408, 768, 7040, 1984, 1344, 1664, 9600, 4480, 3200, 2304, 9344, 7040)
 Y <- ts(y, frequency = 4, start = c(2008, 1))
@@ -54,7 +56,10 @@ plot(component$random,
 par(mfrow=c(1,1))
 
 
+
+# -----------------------------------------
 # Tutorial 5 Q2
+# -----------------------------------------
 
 y <- c(306, 309, 310, 306, 312, 302, 310, 312, 305, 314, 308, 315, 317, 317, 313)
 Y <- ts(y, frequency = 5, start=c(1,1))
@@ -119,3 +124,51 @@ plot(component$random,
      xlab = "Time (Weeks)")
 
 par(mfrow=c(1,1))
+
+
+
+# -----------------------------------------
+# Tutorial 5 Q4
+# -----------------------------------------
+
+y <- c(7.35, 6.65, 9.24, 7.21, 12.60, 21.70, 21.07, 9.80, 8.54, 11.83, 9.31, 16.17, 28.07, 27.16, 12.25, 10.85, 14.84, 11.41, 19.53, 33.53, 32.83) 
+max_k <- length(y)-1
+
+# 4a. Calculate ACF from 0 to n-1 (20)
+autocorrelations <- acf(y, lag.max=max_k, plot=FALSE)
+print(autocorrelations)
+
+# 4b. plot the ACF
+acf(y, 
+    lag.max = max_k,
+    main = "Correlogram of Daily Revenues (RM '000)", 
+    xlab = "Lag k (Days)", 
+    ylab = "Autocorrelation (rk)",
+    lwd = 3,
+    col = "blue"
+)
+
+
+
+# -----------------------------------------
+# ACF/PACF for Tutorial 6 Q3
+# -----------------------------------------
+
+par(mfrow=c(3,2)) # Sets up a grid to view all 6 plots at once
+
+# Q1: Unemployed People (Quarterly)
+y1 <- ts(c(1408, 768, 7040, 1984, 1344, 1664, 9600, 4480, 3200, 2304, 9344, 7040), frequency = 4)
+acf(y1, main="ACF - Q1 Unemployed")
+pacf(y1, main="PACF - Q1 Unemployed")
+
+# Q2: Cash Sales (Daily, 5 days/week)
+y2 <- ts(c(306, 309, 310, 306, 312, 302, 310, 312, 305, 314, 308, 315, 317, 317, 313), frequency = 5)
+acf(y2, main="ACF - Q2 Cash Sales")
+pacf(y2, main="PACF - Q2 Cash Sales")
+
+# Q3: Quarterly Export (Using the 14 known values)
+y3 <- ts(c(2.7, 4.1, 3.9, 3.1, 3.8, 5.4, 5.3, 4.1, 4.9, 6.8, 6.6, 5.0, 5.9, 8.3), frequency = 4)
+acf(y3, main="ACF - Q3 Export")
+pacf(y3, main="PACF - Q3 Export")
+
+par(mfrow=c(1,1)) # Resets the grid
